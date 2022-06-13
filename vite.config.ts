6bab99968cb46ce00262168cbbbf7989cb50ad09
snyx <raphaelcr.ads@gmail.com>
2022-06-13 19:22:14 -0300

    Configured minify build on production...

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { terser } from "rollup-plugin-terser";
+
+const production = !process.env.ROLLUP_WATCH;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    production &&
+      terser({
+        format: {
+          comments: false,
+        },
+      }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
